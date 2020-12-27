fae75dbfccdfdfe7f13ba4c1d78deebc537f4129
Hyuhng Min Kim <minkin918@gmail.com>
2020-12-27 12:27:00 +0900

    removed gitignore

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-// const express = require("express");
-// const { postgraphile } = require("postgraphile");
-
-// const app = express();
-
-// app.use(
-//     postgraphile(
-//         process.env.RDS_URL || "postgres://postgres:ccpklpdb@ccp-klp-db-01.ckftskuwgris.us-east-1.rds.amazonaws.com/witchwatch",
-//         "app",
-//         {
-//             watchPg: true,
-//             graphiql: true,
-//             enhanceGraphiql: true,
-//         }
-//     )
-// );
-
-// app.listen(process.env.PORT || 3000);
-
 const pg = require("pg");
 const { ApolloServer } = require("apollo-server");
 const { makeSchemaAndPlugin } = require("postgraphile-apollo-server");
